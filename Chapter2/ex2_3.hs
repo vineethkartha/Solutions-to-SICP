e0e679274200a9ba9e6f1359c0b43b9b4a5a01c6
vineethkartha <vineeth.kartha@gmail.com>
2018-09-08 19:21:08 +0530

    Solution to 2.3

--- Chapter2/ex2_3.hs
+++ Chapter2/ex2_3.hs
@@ -2,21 +2,33 @@ makePoint x y = (x,y)
 xPoint p = fst p
 yPoint p = snd p
 
--- Rectangle using
+getArea rectangle = (getLength rectangle) * (getBreadth rectangle)
+getPerimeter rectangle = 2 * ((getLength rectangle) + (getBreadth rectangle))
+
+-- First implementation of Rectangle using
 
 --      topLp +-------+    
 --            |       |    
 --            +-------+ botRp
-makeRectangle topLp botRp = (topLp,botRp)
-getTop rectangle = fst rectangle
-getBot rectangle = snd rectangle
-getTopx rectangle = xPoint (getTop rectangle)
-getTopy rectangle = yPoint (getTop rectangle)
-getBotx rectangle = xPoint (getBot rectangle)
-getBoty rectangle = yPoint (getBot rectangle)
-getLength rectangle = abs (getBotx - getTopx)
-getBreadth rectangle = abs (getTopy - getBoty)
+--makeRectangle topLp botRp = (topLp,botRp)
+--getTop rectangle = fst rectangle
+--getBot rectangle = snd rectangle
+--getTopx rectangle = xPoint (getTop rectangle)
+--getTopy rectangle = yPoint (getTop rectangle)
+--getBotx rectangle = xPoint (getBot rectangle)
+--getBoty rectangle = yPoint (getBot rectangle)
+--getLength rectangle = abs (getBotx - getTopx)
+--getBreadth rectangle = abs (getTopy - getBoty)
+--
 
+-- Second implementation of Rectangle using
 
-getArea rectangle = (getLength rectangle) * (getBreadth rectangle)
-getPerimeter rectangle = 2 * ((getLength rectangle) + (getBreadth rectangle))
+--      topLp +-------+    
+--            |       |    
+--            +-------+ 
+
+makeRectangle topLp length breadth = (topLp, (length,breadth))
+getLength rectangle = fst (snd rectangle)
+getBreadth rectangle = snd (snd rectangle)
+getTopx rectangle = xPoint (fst rectangle)
+getTopy rectangle = yPoint (fst rectangle)
